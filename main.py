e1065af7ecfcda6ad1f57dec277b4c0ca2b0fba6
Kelvin Lim <102139658+Kayeel98@users.noreply.github.com>
2022-04-20 15:26:56 +0000

    Update 2.0
    finish game might need to update soon

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import random
 
 name = input("Insert your name here: ")
 
-print("************WELCOME!, {0} TO THE MOO MOO CASINO**************".format(name))
+print()
 
+print("************ WELCOME!, {0} TO THE MOO MOO CASINO **************".format(name))
+
+print()
 
 try:
 	bet = input("Place your bets: ")
@@ -14,9 +17,10 @@ try:
 	 
 
 except ValueError:
-	print("We only accept dollars here")
+	print("We only accept dollars here at MOOMOO Casino")
 	print("")
 	print("Example($ 2)")
+	print()
 	print ("			The following message is brought to you by")
 	print()
 	print ("					PRESS RUN TO TRY AGAIN")
@@ -27,27 +31,68 @@ except ValueError:
 player_winnings = 0 
 house_dice = random.randint(1,12)
 player_dice = random.randint(1,12)
-  
 print()
 
-print("House dice shows ({0})".format(house_dice))
+
+print("MooMoo dice shows ({0})".format(house_dice))
 print("	 VERSUS				")	
-print("Player dice shows ({0})".format(player_dice))
+print("{0} dice shows ({1})".format(name,player_dice))
 
 print()
+
 
 bet_value = bet
 if house_dice > player_dice:
-	print("House wins!, you have lost {0}".format(bet_value))
-	player_winnings += bet
+	print("MooMoo wins!, you have lost {0}".format(bet_value))
+	player_winnings -= bet_value
 elif player_dice > house_dice:
 	print("Player wins!, you have won {0}".format(bet_value))
-	player_winnings = player_winnings - bet
+	player_winnings += bet_value
 else:
 	print("Draw, Nobody wins")
 	player_winnings + 0 
 
-player_winnings = (int(player_winnings))
+player_winnings = int(player_winnings)
 print("Your current winnings are {0}".format(player_winnings))
-x = -20
-print(x)
+
+print()
+
+print("Do you wanna play again ?")
+print("1. YES")
+print("2. NAH ill take the L")
+
+decision = input(int("So what is it gonna be: "))
+
+while decision == 1:
+	print("MooMoo dice shows ({0})".format(house_dice))
+	print("	 VERSUS				")	
+	print("{0} dice shows ({1})".format(name,player_dice))
+	
+	print()
+	
+	
+	bet_value = bet
+	if house_dice > player_dice:
+		print("MooMoo wins!, you have lost {0}".format(bet_value))
+		player_winnings -= bet_value
+	elif player_dice > house_dice:
+		print("Player wins!, you have won {0}".format(bet_value))
+		player_winnings += bet_value
+	else:
+		print("Draw, Nobody wins")
+		player_winnings + 0 
+	
+	player_winnings = int(player_winnings)
+	print("Your current winnings are {0}".format(player_winnings))
+	
+	print()
+	
+	print("Do you wanna play again ?")
+	print("1. YES")
+	print("2. NAH ill take the L")
+	
+	decision = input(int("So what is it gonna be: "))
+	if decision == 0:
+		print("your winning / losing are: {0}".format(player_winnings))
+		print("Thanks for playing~!")
+		break
